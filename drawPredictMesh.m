f843e4a200922f44494372089840563731b03eff
ZackAkil <zackakil94@gmail.com>
2016-02-26 20:35:26 +0000

    added file for drawing meshes of hypothesis

---
+++ drawPredictMesh.m
@@ -0,0 +1,11 @@
+function drawPredictMesh(predictionFunction, X)
+% Draw mesh graph of X values
+%   predictionFunction = function used to generate prediction of hypothesis
+%   X = range of data to be predicted along the x and y axis of the graph
+
+
+
+
+
+
+end
